f6f4ac181aed3e56989c8061159bf41e55b0e09c
Steph18 <stephaneatsu18@gmail.com>
2025-03-20 00:23:01 +0000

    Initialisation du projet

---
+++ src/test/java/com/steph18/demo/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steph18.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
