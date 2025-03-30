0687c78e90c0773757d9965cb7888b10f133448f
Steph18 <stephaneatsu18@gmail.com>
2025-03-30 22:13:59 +0000

    Creation des classes services et repository

---
+++ src/main/java/com/steph18/demo/serviceimpl/GenericServiceImpl.java
@@ -0,0 +1,48 @@
+package com.steph18.demo.serviceimpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public abstract class GenericServiceImpl<T, I>  {
+
+    protected ModelMapper modelMapper = new ModelMapper();
+    public abstract JpaRepository<T, I> getRepository();
+
+    public abstract I getId(T t);
+
+    public T save(T t) {
+        return this.getRepository().save(t);
+    }
+    public void saveAndFlush(T t) {
+        this.getRepository().saveAndFlush(t);
+    }
+    public void saveAll(List<T> tList) {
+        this.getRepository().saveAll(tList);
+    }
+
+    public void saveAllAndFlush(List<T> tList) {
+        getRepository().saveAllAndFlush(tList);
+    }
+
+    public T findById(T t) {
+        Optional<T> opt = this.getRepository().findById(this.getId(t));
+        return opt.orElse(null);
+    }
+    public void getReferenceById(T t) {
+        this.getRepository().getReferenceById(getId(t));
+    }
+    public void delete(T t) {
+        this.getRepository().delete(t);
+    }
+
+    public void deleteById(T t) {
+        this.getRepository().deleteById(getId(t));
+    }
+
+    public void deleteAll(T t) {
+        this.getRepository().deleteAll();
+    }
+}
